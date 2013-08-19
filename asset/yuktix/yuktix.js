ae8b0707686c0c8102c59b23c18b9a74af3e8f0f
saurabh-ar <saurabh@warmachine.(none)>
2013-08-19 18:37:01 +0530

    added progress bar page and modifies yuktix js and css files

--- asset/yuktix/yuktix.js
+++ asset/yuktix/yuktix.js
@@ -1,65 +1,71 @@
+yuktix = {};
 
-		  
-		    
-  yuktix={};
-    
-      yuktix.Ajax = {
-      
-      addSpinner:function(messageDivId) {
-	  $(messageDivId).html('');
-	  var content = '<div> <img src="/asset/css/spinner.gif" alt="spinner"/></div>'  ;
-	  $(messageDivId).html(content);
-	
-      },
+yuktix.Ajax = {
 
-      show: function (messageDivId,content) {
-	  $(messageDivId).html(content);
-      },
+	addSpinner : function(messageDivId, spinner) {
 
-      post:function (dataObj,options) {
-	  console.log("postdata =>" );
-	  /* @imp define all properties that we wish to override */
-	  var defaults = {
-	      type : "POST",
-	      dataType : "json",
-	      timeout : 9000,
-	      onDoneHandler : undefined 
-	  }
+		$(messageDivId).html('')
 
-	  var settings = $.extend({}, defaults, options);
-	  this.addSpinner(settings.messageDivId);
-	  
+		if (spinner) {
 
-	  var xmlRequest = $.ajax({
-	      url: dataObj.endPoint,
-	      type: settings.type ,
-	      dataType: settings.dataType,
-	      data :  dataObj.params,
-	      timeout: settings.timeout,
-	      processData:true
-	  }) ;
+			var content = '<div> <img src="/asset/css/spinner.gif" alt="spinner"/></div>';
+		}
 
-	  xmlRequest.fail(function(response) {
-	      yuktix.Ajax.show(settings.messageDivId,response);
-	  });
+		else {
 
-	  xmlRequest.done(function(response) {
-	      console.log("response =>" + response);
-	      
-	      if(settings.dataType == 'text') {
-		  yuktix.Ajax.show(settings.messageDivId,response);
-	      }
-	      
-	      if(settings.dataType == 'json') {
-		  yuktix.Ajax.show(settings.messageDivId,response.message);
-	      }
+			var content = "<div class='progress progress-striped active'><div class='progress-bar' role='progressbar' aria-valuenow='100'\
+	    	aria-valuemin='0' aria-valuemax='100' style='width: 100%;'><span class='sr-only'>please wait</span></div></div>";
+		}
 
-	      if(typeof settings.onDoneHandler !== "undefined") {
-		  settings.onDoneHandler(dataObj,response);
-	      }
-	  }) ;
-	  
-      }
+		$(messageDivId).html(content);
 
+	},
 
-  }
+	show : function(messageDivId, content) {
+		$(messageDivId).html(content);
+	},
+
+	post : function(dataObj, options, spinner) {
+		console.log("postdata =>");
+		/* @imp define all properties that we wish to override */
+		var defaults = {
+			type : "POST",
+			dataType : "json",
+			timeout : 9000,
+			onDoneHandler : undefined
+		}
+
+		var settings = $.extend({}, defaults, options);
+		this.addSpinner(settings.messageDivId, spinner);
+
+		var xmlRequest = $.ajax({
+			url : dataObj.endPoint,
+			type : settings.type,
+			dataType : settings.dataType,
+			data : dataObj.params,
+			timeout : settings.timeout,
+			processData : true
+		});
+
+		xmlRequest.fail(function(response) {
+			yuktix.Ajax.show(settings.messageDivId, response);
+		});
+
+		xmlRequest.done(function(response) {
+			console.log("response =>" + response);
+
+			if (settings.dataType == 'text') {
+				yuktix.Ajax.show(settings.messageDivId, response);
+			}
+
+			if (settings.dataType == 'json') {
+				yuktix.Ajax.show(settings.messageDivId, response.message);
+			}
+
+			if (typeof settings.onDoneHandler !== "undefined") {
+				settings.onDoneHandler(dataObj, response);
+			}
+		});
+
+	}
+}
